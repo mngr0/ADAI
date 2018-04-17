903a84067904971025d176ba8bcb62546cb0d41a
mngr0 <ngrmarco@libero.it>
2018-04-17 15:57:11 +0200

    removed gridlayoutt

--- app/src/main/java/com/example/marco/myfeeder/BluetoothConnect.java
+++ app/src/main/java/com/example/marco/myfeeder/BluetoothConnect.java
@@ -3,37 +3,10 @@ package com.example.marco.myfeeder;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 
 
 public class BluetoothConnect extends AppCompatActivity {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-/*
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.bluetooth_connect);
 
-
-         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        // specify an adapter (see also next example)
-        String [] test={"we","bubba"};
-        mAdapter = new MyAdapter(test);
-        mRecyclerView.setAdapter(mAdapter);
-
-    }
-*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +20,4 @@ public class BluetoothConnect extends AppCompatActivity {
             transaction.commit();
         }
     }
-
-
-
-
 }

1537a54550c79c7a977c5355283e99c02dbbc35b
mngr0 <ngrmarco@libero.it>
2018-04-17 14:50:25 +0200

    more files, wubba lubba

--- app/src/main/java/com/example/marco/myfeeder/MainActivity.java
+++ app/src/main/java/com/example/marco/myfeeder/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.marco.myfeeder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void showBLE(View view) {
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Hello toast!", Toast.LENGTH_SHORT);
+        toast.show();
+
+        Intent intent = new Intent(this, BluetoothConnect.class);
+        startActivity(intent);
+    }
+
+
 }

37d24c2c707c8cc4dad1c97fcc0e628486115ea0
mngr0 <ngrmarco@libero.it>
2018-04-26 14:55:38 +0200

    setting swappable

--- app/src/main/java/com/example/marco/myfeeder/settings/FormatSetting.java
+++ app/src/main/java/com/example/marco/myfeeder/settings/FormatSetting.java
@@ -1,30 +1,59 @@
 package com.example.marco.myfeeder.settings;
 
 
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.marco.myfeeder.R;
 
-public class FormatSetting extends FragmentActivity implements FragmentEncoderFormat.OnFragmentInteractionListener {
+public class FormatSetting extends FragmentActivity {
+
+    String state;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        state="Encoder";
         setContentView(R.layout.format_setting);
+
+        showEncoder();
+
+    }
+    public void showEncoder(){
         FragmentEncoderFormat fef = new FragmentEncoderFormat();
+        android.app.FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.host_frame, fef);
+        transaction.commit();
+        findViewById(R.id.buttonEncoder).setBackgroundColor(Color.GREEN);
+        findViewById(R.id.buttonTimer).setBackgroundColor(Color.RED);
+        state="Encoder";
+    }
+
+    public void showTimer(){
+        FragmentTimerFormat fef = new FragmentTimerFormat();
         android.app.FragmentTransaction transaction = getFragmentManager().beginTransaction();
         transaction.replace(R.id.host_frame, fef);
 
         transaction.commit();
-
+        findViewById(R.id.buttonEncoder).setBackgroundColor(Color.RED);
+        findViewById(R.id.buttonTimer).setBackgroundColor(Color.GREEN);
+        state="Timer";
     }
 
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
+    public void buttonSwap(View view){
+        if(state.equals("Encoder")){
+            showTimer();
+        }else{
+            showEncoder();
+        }
 
     }
+
 }
--- app/src/main/java/com/example/marco/myfeeder/settings/FragmentEncoderFormat.java
+++ app/src/main/java/com/example/marco/myfeeder/settings/FragmentEncoderFormat.java
@@ -1,98 +1,32 @@
 package com.example.marco.myfeeder.settings;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.marco.myfeeder.format_edit.FormatAdapter;
+
 import com.example.marco.myfeeder.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FragmentEncoderFormat.OnFragmentInteractionListener} interface
- * to handle interaction events.
- */
 public class FragmentEncoderFormat extends Fragment {
 
-   // protected RecyclerView mRecyclerView;
-    protected FormatAdapter mAdapter;
-    protected RecyclerView.LayoutManager mLayoutManager;
-    protected String[] mDataset;
-    private OnFragmentInteractionListener mListener;
-
     public FragmentEncoderFormat() {   }
-
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mDataset = new String[10];
-        for (int i = 0; i < 10; i++) {
-            mDataset[i] = "" + i;
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View rootView = inflater.inflate(R.layout.fragment_encoder_format, container, false);
 
-      //  mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView2);
-        mLayoutManager = new LinearLayoutManager(getActivity());
-        mAdapter = new FormatAdapter(mDataset);
-        //mRecyclerView.setAdapter(mAdapter);
         return rootView;
     }
 
-
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }

34688d6a3b438ff829eb07eaf4832a3e115f7877
mngr0 <ngrmarco@libero.it>
2018-04-26 16:55:02 +0200

    no comments for awesomness

--- app/src/main/java/com/example/marco/myfeeder/FormatSelection.java
+++ app/src/main/java/com/example/marco/myfeeder/FormatSelection.java
@@ -1,32 +1,105 @@
 package com.example.marco.myfeeder;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.RadioGroup;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.marco.myfeeder.format_edit.FormatEdit;
-import com.example.marco.myfeeder.settings.FormatSetting;
 
 public class FormatSelection extends AppCompatActivity {
+
+    static final int size = 8;
+    private static View[] viewArray = new View[size];
+    private static int activeRadio;
+
+    public static void setRadio(int index) {
+        if (index == activeRadio) {
+            return;
+        }
+        for (int i = 0; i < size; i++) {
+            if (i != index) {
+                ((RadioButton) viewArray[i].findViewById(R.id.radioButton3)).setChecked(false);
+            } else {
+                ((RadioButton) viewArray[i].findViewById(R.id.radioButton3)).setChecked(true);
+            }
+        }
+        activeRadio = index;
+    }
+
+    public class ButtonListener implements View.OnClickListener {
+
+        int mIndex;
+        Context mContext;
+        FormatSelection mParent;
+
+        public ButtonListener(int index, Context context, FormatSelection parent) {
+            mIndex = index;
+            mContext = context;
+            mParent = parent;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast toast = Toast.makeText(mContext, "Element " + mIndex + " clicked.", Toast.LENGTH_SHORT);
+            toast.show();
+            mParent.showFormatEdit(mIndex);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Toast toast = Toast.makeText(getApplicationContext(), "BAAACK!", Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+    public static class RadioListener implements View.OnClickListener {
+
+        int mIndex;
+        Context mContext;
+
+        public RadioListener(int index, Context context) {
+            mIndex = index;
+            mContext = context;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast toast = Toast.makeText(mContext, "Element " + mIndex + " clicked.", Toast.LENGTH_SHORT);
+            toast.show();
+            FormatSelection.setRadio(mIndex);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.format_selection);
-
-        //RadioGroup rg = new RadioGroup(getApplicationContext());
-        //rg.addView(findViewById(R.id.radioButton));
-        //rg.addView(findViewById(R.id.radioButton2));
-
+        final Context t = getApplicationContext();
+        setContentView(R.layout.activity_format_selection);
+        LinearLayout list = (LinearLayout) findViewById(R.id.layoutConfList);
+        for (int i = 0; i < size; i++) {
+            viewArray[i] = getLayoutInflater().inflate(R.layout.item_selection, null, false);
+            list.addView(viewArray[i]);
+            viewArray[i].findViewById(R.id.button19).setOnClickListener(new ButtonListener(i, t, this));
+            viewArray[i].findViewById(R.id.radioButton3).setOnClickListener(new RadioListener(i, t));
+            ((TextView) viewArray[i].findViewById(R.id.textView4)).setText("conf" + i);
+        }
     }
-    public void showFormatEdit(View view) {
+
+    public void showFormatEdit(int index) {
 
         Toast toast = Toast.makeText(getApplicationContext(), "go format_setting!", Toast.LENGTH_SHORT);
         toast.show();
-
         Intent intent = new Intent(this, FormatEdit.class);
-        startActivity(intent);
+        startActivityForResult(intent, 42);
+
     }
 }
--- app/src/main/java/com/example/marco/myfeeder/settings/FragmentEncoderFormat.java
+++ app/src/main/java/com/example/marco/myfeeder/settings/FragmentEncoderFormat.java
@@ -1,17 +1,25 @@
 package com.example.marco.myfeeder.settings;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.example.marco.myfeeder.R;
 
 
 public class FragmentEncoderFormat extends Fragment {
+
+
+
+
 
     public FragmentEncoderFormat() {   }
 
